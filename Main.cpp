690a70bb0759da7c76bdfda174e13f135252ca7e
Isaac  Appiagyei <ikwaku465@gmail.com>
2024-06-16 10:13:39 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int myInt(0);
+	int count(0);
+
+	while (count <= 10) 
+	{
+		cout << myInt << endl;
+		myInt++;
+		count++;
+	}
+
+	return 0;
+}
